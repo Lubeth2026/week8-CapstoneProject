393c41c35016409e6ad0ff8ae09936c3eca539b7
Lubeth <lubethjimenez@yahoo.com>
2026-03-22 13:15:09 -0400

    Add workers API to function to capstone project

--- index.js
+++ index.js
@@ -21,17 +21,54 @@ async function getKey() {
         }
 
         const {key} = await res.json();
-        console.log(key)
+        //console.log(key)
         return key
     } catch (error) {
         console.error("Didn't get the key")
     }
 }
-//End of Workers Proxy-Key Function// 
+//End of Proxy-Key Function// 
+
+//Get Workers AI API//POST Request//
+async function getTheAPI(url, options) {
+    try {
+        const res = await fetch(url, options);
+
+        if(!res.ok){
+            throw new Error("Didn't fetch the Data")
+        }
+
+        const {result} = await res.json();
+        return result
+    } catch (error) {
+        
+    }
+}
+//End of Workers API Function//
 
 async function main() {
     try {
-        const key = await getKey() 
+        informationArea.addEventListener("submit", async(event)=>{
+        event.preventDefault()
+        const key = await getKey()
+        const corsURL = "https://corsproxy.io/?url="; 
+        const workersEndpoint = "https://api.cloudflare.com/client/v4/accounts/82bd1d2acadf816a51be4a65b1c64317/ai/run/@cf/meta/llama-3-8b-instruct";
+        const url = corsURL + workersEndpoint
+        const promptBody = {
+          messages: [
+            { role: "system", content: "You are a used car sales rep, trying to make sales on four vehicles, one truck, one suv, and two sedans. A 2026 chevy silverado, color white, with 20,000 miles, no accidents, also 2021 nissan rogue, color black, with 70,000 miles no accidents. Two sedans a 2019 nissan altima, color white, 60,000 miles with no accidents, also a 2019 hyundai accent, color black, with 10,000 nothing wrong with it. You do not go off topic, you only tell the products or info about what a product is. Never respond with more than 15 words, and never list every single product, perhaps ask a question."},
+            { role: "user", content: "What products do you sell? "},
+          ],
+        };
+        const options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json", Authorization: `Bearer ${key}` },
+            body: JSON.stringify(promptBody),
+        };
+        //const {response} = await getTheAPI(url, options);
+        const response = "";
+        console.log(response)
+    })
     } catch (error) {
         console.log(error)
     }
